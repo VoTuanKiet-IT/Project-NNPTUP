e83be230d55c34ba5cfdbc23bf0abe8053cbf790
VoTuanKiet-IT <it.tuankiet58@gmail.com>
2025-10-26 03:14:11 +0700

    add ViewLogs

--- server/models/Star.js
+++ server/models/Star.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
-const PostSchema = new Schema({
+
+const StarSchema = new Schema({
   postId: {
     type: Schema.Types.ObjectId,
     ref: 'Post',
